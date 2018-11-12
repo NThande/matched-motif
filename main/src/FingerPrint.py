7ca899fe185de14e3d6514290c7c87e6b3b02398
Njuguna Thande <nthande@princeton.edu>
2018-11-12 13:07:51 -0500

    New threshold-checking code

--- main/src/FingerPrint.py
+++ main/src/FingerPrint.py
@@ -28,31 +28,18 @@ def visualize_stft(f, t, Zxx):
     plt.xlabel('Time [sec]')
     plt.show(block=False)
 
-def visualize_peaks(f, t, x, y):
+
+def visualize_peaks(f, t, peaks, threshold):
+    y, x = np.where(peaks != 0)
     plt.figure()
     plt.plot(t[x], f[y], 'rx')
-    plt.title('STFT Peaks')
+    plt.title('STFT Peaks, Threshold = {}'.format(threshold))
     plt.ylabel('Frequency [Hz]')
     plt.xlabel('Time [sec]')
     plt.show(block=False)
 
 
-# Coarse segmentation for peak identification
-def segment_coarse(f, t, Zxx):
-    f_zone = 3
-    t_zone = 3
-    f_len = f.shape[0]
-    t_len = t.shape[0]
-    for freq in range(0, f_zone, f_len):
-        for time in range(0, t_zone, t.shape[0]):
-            f_end = freq + f_zone if freq + f_zone < f_len else f_len
-            t_end = time + t_zone if time + t_zone < t_len else t_len
-            peaks = find_peaks_shift(f[freq: f_end], t[time: t_end],
-                                     Zxx[freq: f_end, time: t_end])
-
-    return None
-
-
+# Find the index of the value closest to a0 in a
 def find_nearest_index(a, a0):
         "Index in nd array `a` closest to the scalar value `a0`"
         idx = np.abs(a - a0).argmin(axis=None)
@@ -79,9 +66,10 @@ def find_peaks_shift(f, t, Zxx, threshold=1.0):
     spect_peaks = np.copy(spect)
     spect_peaks[peaks != True] = 0
 
-    # Time-Frequency segmentation
+    # Coarse Segmentation: Split into equally spaced zones
     fMax = f.shape[0]
     tMax = t.shape[0]
+
     # Aim for 2-second, 5000 Hz windows
     fInc = find_nearest_index(f, 2000)[0]
     tInc = find_nearest_index(t, 1.5)[0]
@@ -112,54 +100,10 @@ def find_peaks_shift(f, t, Zxx, threshold=1.0):
             spect_window[spect_window < peaks_thresh] = 0
             # print(np.sum(spect_peaks != 0))
 
-    # Prune by amplitude
-   # spect_peaks = np.copy(spect)
-    #spect_peaks[peaks != True] = 0
-
-    #peaks_thresh = np.max(spect_peaks) * threshold
-   # spect_peaks[spect_peaks < peaks_thresh] = 0
     print(np.sum(spect_peaks != 0))
-
-    y, x = np.where(spect_peaks != 0)
-    visualize_stft(f, t, spect_peaks)
-    visualize_peaks(f, t, x, y)
-
     return spect_peaks
 
 
-def find_peaks_filt(f, t, Zxx, threshold=100):
-    pzxx = np.abs(Zxx)
-    neighborhood_size = [5, 1]
-
-    # Find all peaks in the zone
-    data_max = filters.maximum_filter(pzxx, neighborhood_size)
-    maxima = (pzxx == data_max)
-    data_min = filters.minimum_filter(pzxx, neighborhood_size)
-
-    # Prune by amplitude
-    diff = ((data_max - data_min) > threshold)
-    maxima[diff == 0] = 0
-    visualize_stft(f, t, maxima)
-
-    # Merge Peaks from zones into a single vector
-    labeled, num_objects = ndimage.label(maxima)
-    print(num_objects)
-    slices = ndimage.find_objects(labeled)
-    x, y = [], []
-    for dy, dx in slices:
-        x_center = (dx.start + dx.stop - 1) / 2
-        x.append(x_center)
-        y_center = (dy.start + dy.stop - 1) / 2
-        y.append(y_center)
-
-    plt.figure()
-    plt.autoscale(True)
-    plt.plot(x, y, 'rx')
-
-    return x, y
-
-
-# Coarse Segmentation: Split into equally spaced zones
 # Anchor Point Hashing
 # For each peak:
 # Set the target zone
@@ -176,6 +120,9 @@ for i in range(0, 1):
     sound, r = read_audio('./main/bin/t{}.wav'.format(i + 1))
     f, t, Zxx = transform_stft(sound, r, 10000)
     visualize_stft(f, t, Zxx)
-    find_peaks_shift(f, t, Zxx)
-    # find_peaks_filt(f, t, Zxx)
+    kRange = np.linspace(0, 1, 10)
+    for k in kRange:
+        peaks = find_peaks_shift(f, t, Zxx, k)
+        # visualize_stft(f, t, peaks)
+        visualize_peaks(f, t, peaks, k)
 plt.show()
