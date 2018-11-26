78767945db30558a6c7c19096586f8926c5b66a6
Njuguna Thande <nthande@princeton.edu>
2018-11-26 04:07:35 -0500

    Using dejavu code as model

--- main/src/FingerPrint.py
+++ main/src/FingerPrint.py
@@ -1,7 +1,110 @@
+import hashlib
+
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
 from scipy.io.wavfile import read
+from scipy.ndimage.filters import maximum_filter
+from scipy.ndimage.morphology import (generate_binary_structure,
+                                      iterate_structure, binary_erosion)
+
+# Lifted from Dejavu Fingerprinting System
+IDX_FREQ_I = 0
+IDX_TIME_J = 1
+
+######################################################################
+# Sampling rate, related to the Nyquist conditions, which affects
+# the range frequencies we can detect.
+DEFAULT_FS = 44100
+
+######################################################################
+# Size of the FFT window, affects frequency granularity
+DEFAULT_WINDOW_SIZE = 4096
+
+######################################################################
+# Ratio by which each sequential window overlaps the last and the
+# next window. Higher overlap will allow a higher granularity of offset
+# matching, but potentially more fingerprints.
+DEFAULT_OVERLAP_RATIO = 0.5
+
+######################################################################
+# Degree to which a fingerprint can be paired with its neighbors --
+# higher will cause more fingerprints, but potentially better accuracy.
+DEFAULT_FAN_VALUE = 15
+
+######################################################################
+# Minimum amplitude in spectrogram in order to be considered a peak.
+# This can be raised to reduce number of fingerprints, but can negatively
+# affect accuracy.
+DEFAULT_AMP_MIN = 10
+
+######################################################################
+# Number of cells around an amplitude peak in the spectrogram in order
+# for Dejavu to consider it a spectral peak. Higher values mean less
+# fingerprints and faster matching, but can potentially affect accuracy.
+PEAK_NEIGHBORHOOD_SIZE = 20
+
+######################################################################
+# Thresholds on how close or far fingerprints can be in time in order
+# to be paired as a fingerprint. If your max is too low, higher values of
+# DEFAULT_FAN_VALUE may not perform as expected.
+MIN_HASH_TIME_DELTA = 0
+MAX_HASH_TIME_DELTA = 200
+
+######################################################################
+# If True, will sort peaks temporally for fingerprinting;
+# not sorting will cut down number of fingerprints, but potentially
+# affect performance.
+PEAK_SORT = True
+
+def fingerprint(channel_samples, Fs=DEFAULT_FS,
+                wsize=DEFAULT_WINDOW_SIZE,
+                wratio=DEFAULT_OVERLAP_RATIO,
+                fan_value=DEFAULT_FAN_VALUE,
+                amp_min=DEFAULT_AMP_MIN):
+    """
+    FFT the channel, log transform output, find local maxima, then return
+    locally sensitive hashes.
+    """
+    # FFT the signal and extract frequency components
+    spect = plt.mlab.specgram(
+        channel_samples,
+        NFFT=wsize,
+        Fs=Fs,
+        window=plt.mlab.window_hanning,
+        noverlap=int(wsize * wratio))[0]
+
+    # apply log transform since specgram() returns linear array
+    spect = 10 * np.log10(spect)
+    spect[spect == -np.inf] = 0  # replace infs with zeros
+
+    # find local maxima
+    local_maxima = get_2D_peaks(spect, plot=False, amp_min=amp_min)
+
+    # return hashes
+    return generate_hashes(local_maxima, fan_value=fan_value)
+
+def generate_hashes(peaks, fan_value=DEFAULT_FAN_VALUE):
+    """
+    Hash list structure:
+       sha1_hash[0:20]    time_offset
+    [(e05b341a9b77a51fd26, 32), ... ]
+    """
+    for i in range(len(peaks)):
+        for j in range(1, fan_value):
+            if (i + j) < len(peaks):
+
+                freq1 = peaks[i][IDX_FREQ_I]
+                freq2 = peaks[i + j][IDX_FREQ_I]
+                t1 = peaks[i][IDX_TIME_J]
+                t2 = peaks[i + j][IDX_TIME_J]
+                t_delta = t2 - t1
+
+                if t_delta >= MIN_HASH_TIME_DELTA and t_delta <= MAX_HASH_TIME_DELTA:
+                    h = hashlib.sha1(
+                        "%s|%s|%s" % (str(freq1), str(freq2), str(t_delta)))
+                    yield (h.hexdigest()[0:FINGERPRINT_REDUCTION], t1)
+# End citation
 
 
 # Read audio as a time-signal
@@ -13,6 +116,7 @@ def read_audio(filename):
 # Transform audio to Time-Frequency Domain with STFT
 def transform_stft(sound, fs, seg_len=1000):
     f, t, zxx = signal.stft(sound, fs, nperseg=seg_len)
+    zxx = 10 * np.log10(zxx)
     return f, t, zxx
 
 
@@ -25,7 +129,7 @@ def visualize_stft(f, t, zxx):
     plt.show(block=False)
 
 
-def visualize_peaks(f, t, peaks, threshold=-1):
+def plot_peaks(f, t, peaks, threshold=-1):
     y, x = np.where(peaks != 0)
     plt.figure()
     plt.plot(t[x], f[y], 'rx')
@@ -39,8 +143,8 @@ def visualize_peaks(f, t, peaks, threshold=-1):
     plt.show(block=False)
 
 
-def visualize_peak_pairs(f, t, peaks, pairs, inc=50):
-    visualize_peaks(f, t, peaks)
+def plot_peak_pairs(f, t, peaks, pairs, inc=50):
+    plot_peaks(f, t, peaks)
     count = 0
     f_cur = 0
     t_cur = 0
@@ -128,10 +232,50 @@ def find_peaks_shift(f, t, zxx, threshold=1.0):
     num_peaks = np.sum(spect_peaks != 0)
     return spect_peaks, num_peaks
 
+# Lifted from Dejavu fingerprinting system
+def get_2D_peaks(spect, plot=False, amp_min=DEFAULT_AMP_MIN):
+    # http://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.morphology.iterate_structure.html#scipy.ndimage.morphology.iterate_structure
+    struct = generate_binary_structure(2, 1)
+    neighborhood = iterate_structure(struct, PEAK_NEIGHBORHOOD_SIZE)
+
+    # find local maxima using our fliter shape
+    local_max = maximum_filter(spect, footprint=neighborhood) == spect
+    background = (spect == 0)
+    eroded_background = binary_erosion(background, structure=neighborhood,
+                                       border_value=1)
+
+    # Boolean mask of arr2D with True at peaks
+    detected_peaks = local_max - eroded_background
+
+    # extract peaks
+    amps = spect[detected_peaks]
+    j, i = np.where(detected_peaks)
+
+    # filter peaks
+    amps = amps.flatten()
+    peaks = zip(i, j, amps)
+    peaks_filtered = [x for x in peaks if x[2] > amp_min]  # freq, time, amp
+
+    # get indices for frequency and time
+    frequency_idx = [x[1] for x in peaks_filtered]
+    time_idx = [x[0] for x in peaks_filtered]
+
+    if plot:
+        # scatter of the peaks
+        fig, ax = plt.subplots()
+        ax.imshow(spect)
+        ax.scatter(time_idx, frequency_idx)
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Frequency')
+        ax.set_title("Spectrogram")
+        plt.gca().invert_yaxis()
+        plt.show()
+
+    return zip(frequency_idx, time_idx)
 
 # Anchor Point Pairing
 # Pair peaks with each other
-def pair_peaks(peaks, fanout=3):
+def get_peak_pairs(peaks, fanout=3):
     pairs = np.zeros((0, 4))
     fidx, tidx = np.where(peaks != 0)
 
@@ -143,7 +287,7 @@ def pair_peaks(peaks, fanout=3):
         ftz_idx = np.where(
             np.logical_and(fidx >= fidx[i] - ftz_max, fidx <= fidx[i] + ftz_max))  # Frequency target zone
         ttz_idx = np.where(
-            np.logical_and(tidx >= tidx[i] + 1, tidx <= tidx[i] + ttz_max))  # Frequency target zone
+            np.logical_and(tidx >= tidx[i] + 1, tidx <= tidx[i] + ttz_max))  # Time target zone
         tzone_idx = np.intersect1d(ftz_idx, ttz_idx)
 
         # Pair a fixed number of peaks within the target zone
@@ -159,7 +303,7 @@ def pair_peaks(peaks, fanout=3):
 # Anchor Point Hashing
 # For each pair:
 # Create a hash for each pair
-# Merge hashes into a single database
+# Move hashes into database
 def convert_to_hashes(pairs):
     hashtable = {}
     for pV in pairs:
@@ -168,6 +312,7 @@ def convert_to_hashes(pairs):
         time_stamp = pV[2]
         hash = np.round(np.abs(f_diff) * 1000000) + np.round(np.abs(t_diff) * 1000) + pV[0]
         hashtable[hash] = time_stamp
+        print(hash)
     return hashtable
 
 
@@ -176,7 +321,7 @@ def convert_to_hashes(pairs):
 # Record results of hash table fingerprints
 # Timestamp extraction
 
-for i in range(3, 4):
+for i in range(0, 1):
     sound, r = read_audio('./main/bin/t{}.wav'.format(i + 1))
     f, t, zxx = transform_stft(sound, r, 10000)
     visualize_stft(f, t, zxx)
@@ -188,9 +333,9 @@ for i in range(3, 4):
     #     visualize_peaks(f, t, peaks, k)
     peaks, num_peaks = find_peaks_shift(f, t, zxx, 1.0)
     print("Number of peaks: {}".format(num_peaks))
-    visualize_peaks(f, t, peaks)
-    pairs, num_pairs = pair_peaks(peaks)
-    visualize_peak_pairs(f, t, peaks, pairs)
+    plot_peaks(f, t, peaks)
+    pairs, num_pairs = get_peak_pairs(peaks)
+    plot_peak_pairs(f, t, peaks, pairs)
     print("Number of peak pairs: {}".format(num_pairs))
     pair_table = convert_to_hashes(pairs)
     print("Number of unique hashes: {}".format(len(pair_table)))
