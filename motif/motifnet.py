93d32759fa23bf2976d2cd975efc68d0b72786f0
nthan <nthande@outlook.com>
2019-02-24 17:42:21 -0500

    reverted previous main change

--- motif/motifnet.py
+++ motif/motifnet.py
@@ -53,42 +53,36 @@ def clustering(g):
     return nx.clustering(g)
 
 
-
 def main():
     name = 't3_train'
     directory = "./bin/labelled"
     audio, fs = fileutils.load_audio(name, audio_dir=directory)
     length = 3
 
-    # Draw a labeled network graph, arc graph, and chord graph for a given graph
-    def display_demo(g, node_color='b', label_name='Label'):
-        chord_labels, arc_labels = vis._create_node_labels(g, label_col=label_name, node_attr=label_name)
-        ax_net = vis.draw_netgraph(g, node_color=node_color)
-        c = vis.draw_chordgraph(g,
-                                node_labels=chord_labels,
-                                label_col='Time',
-                                title='Chord Graph of Regular Segmentation')
-        ax_arc = vis.draw_arcgraph(g,
-                                   node_size=30.,
-                                   node_labels=arc_labels,
-                                   node_order=range(0, nx.number_of_nodes(g)),
-                                   node_color=node_color)
-        return ax_net, ax_arc, c
-
     # # Regular Segmentation
-    _, _, segments_reg, adj_reg = mf.thumbnail(audio, fs, length=length)
+    # _, _, segments_reg, adj_reg = mf.thumbnail(audio, fs, length=length)
+    #
+    # # Format segment labels
+    # reg_labels = []
+    # for i in range(segments_reg.shape[0]):
+    #     reg_labels.append('%2.2f - %2.2f' % (segments_reg[i], segments_reg[i] + length))
+    #
+    # adj_reg[adj_reg < 50.] = 0
+    # G_reg = adjacency_to_graph(adj_reg, reg_labels, 'Time', prune=True)
 
-    # Format segment labels
-    reg_labels = []
-    for i in range(segments_reg.shape[0]):
-        reg_labels.append('%2.2f - %2.2f' % (segments_reg[i], segments_reg[i] + length))
-
-    adj_reg[adj_reg < 50.] = 0
-    G_reg = adjacency_to_graph(adj_reg, reg_labels, 'Time', prune=True)
-
-    net_reg, arc_reg, c_reg = display_demo(G_reg, node_color='b', label_name='Time')
-    net_reg.set_title("Network graph of Regular Segmentation")
-    arc_reg.set_title("Time-Ordered ArcGraph of Regular Segmentation")
+    # chord_labels, arc_labels = vis._create_node_labels(G_reg, label_col='Time', node_attr='Time')
+    # ax = vis.draw_netgraph(G_reg, node_color='b')
+    # ax.set_title("Network graph of Regular Segmentation")
+    # c_reg = vis.draw_chordgraph(G_reg,
+    #                             node_labels=chord_labels,
+    #                             label_col='Time',
+    #                             title='Chord Graph of Regular Segmentation')
+    # ax = vis.draw_arcgraph(G_reg,
+    #                        node_size=30.,
+    #                        node_labels=arc_labels,
+    #                        node_order=range(0, nx.number_of_nodes(G_reg)))
+    # ax.set_title("Time-Ordered ArcGraph of Regular Segmentation")
+    # vis.show(c_reg)
 
     # Onset Segmentation
     _, _, segments_onset, adj_onset = mf.thumbnail(audio, fs, length=length, seg_method='onset')
@@ -103,13 +97,24 @@ def main():
     G_onset = adjacency_to_graph(adj_onset, onset_labels, 'Time', prune=False)
     Gp_onset = prune_graph(G_onset)
 
-    net_onset, arc_onset, c_onset = display_demo(Gp_onset, node_color='b', label_name='Time')
-    net_onset.set_title("Network graph of Onset Segmentation")
-    arc_onset.set_title("Time-Ordered ArcGraph of Onset Segmentation")
 
-    vis.show(c_onset)
-    vis.show(c_reg)
-    vis.show()
+    # Display onset segmentation graph
+    # chord_labels, arc_labels = vis._create_node_labels(Gp_onset, label_col='Time', node_attr='Time')
+    # ax = vis.draw_netgraph(G_onset, node_color='b')
+    # ax.set_title("Network graph of Onset Segmentation")
+    # c_onset = vis.draw_chordgraph(Gp_onset,
+    #                               node_labels=chord_labels,
+    #                               label_col='Time',
+    #                               title='Chord Graph Of Onset Segmentation')
+    # ax = vis.draw_arcgraph(Gp_onset,
+    #                        node_size=30.,
+    #                        node_order=range(0, nx.number_of_nodes(Gp_onset)),
+    #                        node_labels=arc_labels
+    #                        )
+    # ax.set_title("Time-Ordered ArcGraph of Onset Segmentation")
+    # vis.show(c_onset)
+
+    # vis.show()
 
 
 if __name__ == '__main__':
